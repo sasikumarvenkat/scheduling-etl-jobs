1c3e635c50ae9c4a9745dd68f7e3a16474e653c0
Sasikumar Venkatesh <sasikumar.venkat@gmail.com>
2018-02-06 15:57:34 +0530

    [Scheduling-Spark] Scheduling Apache Spark Pi Using Spring Scheduler

--- src/main/java/com/example/automation/schedulingetljobs/SchedulingEtlJobsApplication.java
+++ src/main/java/com/example/automation/schedulingetljobs/SchedulingEtlJobsApplication.java
@@ -8,7 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 public class SchedulingEtlJobsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SchedulingEtlJobsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulingEtlJobsApplication.class, args);
+    }
 }
--- src/main/java/com/example/automation/schedulingetljobs/taks/ScheduledTasks.java
+++ src/main/java/com/example/automation/schedulingetljobs/taks/ScheduledTasks.java
@@ -1,49 +1,59 @@
 package com.example.automation.schedulingetljobs.taks;
 
-import java.io.IOException;
-
+import com.example.automation.schedulingetljobs.constants.SchedulerConstants;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.apache.spark.launcher.SparkLauncher;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import com.example.automation.schedulingetljobs.constants.SchedulerConstants;
+import java.io.IOException;
 
 @Component
 public class ScheduledTasks {
 
-	private static final Logger LOGGER = LogManager.getLogger(ScheduledTasks.class);
+    private static final Logger LOGGER = LogManager.getLogger(ScheduledTasks.class);
 
-	@Scheduled(fixedDelay = 1000)
-	public void testScheduled() {
-		LOGGER.info("Info: Test Method Scheduled on Fixed Delay");
-	}
+    @Scheduled(fixedDelay = 1000)
+    public void testScheduled() {
+        LOGGER.info("Info: Test Method Scheduled on Fixed Delay");
+    }
 
-	@Scheduled(cron = SchedulerConstants.CRON_EXPRESSION_EVERY_MINUTE)
-	public void sampleCronScheduled() {
-		LOGGER.info("Info: Sample Test Method Scheduled based on CRON Job");
-	}
+    @Scheduled(cron = SchedulerConstants.CRON_EXPRESSION_EVERY_MINUTE)
+    public void sampleCronScheduled() {
+        LOGGER.info("Info: Sample Test Method Scheduled based on CRON Job");
+    }
 
-	@Scheduled(fixedDelay = 2000)
-	public void scheduleSparkETLJob() {
-		LOGGER.info("Info: Scheduling the Apache Spark ETL workloads");
-		Process spark;
-		try {
-			spark = new SparkLauncher().setAppResource("/usr/local/spark/examples/jar/spark*.jar")
-					.setMainClass("org.apache.spark.examples.SparkPi").setMaster("local").setAppName("Spark Pi")
-					.addSparkArg("100").setConf(SparkLauncher.DRIVER_MEMORY, "2g").launch();
+    @Scheduled(fixedDelay = 2000)
+    public void scheduleSparkPiApp() {
+        LOGGER.info("Info: Scheduling the Apache Spark ETL workloads");
+        Process spark;
+        try {
+            spark = new SparkLauncher().setAppResource("//usr/local/spark/examples/jars/spark-examples_2.11-2.2.1.jar")
+                    .setMainClass("org.apache.spark.examples.SparkPi").setMaster("local").setAppName("Spark Pi")
+                    .setConf(SparkLauncher.DRIVER_MEMORY, "1g").addAppArgs("100").launch();
+            LOGGER.info("Spark Pi Started.");
+            InputStreamReaderRunnable inputStreamReaderRunnable = new InputStreamReaderRunnable(spark.getInputStream(), "input");
+            Thread inputThread = new Thread(inputStreamReaderRunnable, "LogStreamReader input");
+            inputThread.start();
 
-			spark.waitFor();
-			LOGGER.info("Spark Job Is Alive: " + spark.isAlive());
-		} catch (IOException e) {
-			LOGGER.info(e);
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			LOGGER.info(e);
-			e.printStackTrace();
-		}
+            InputStreamReaderRunnable errorStreamReaderRunnable = new InputStreamReaderRunnable(spark.getErrorStream(), "error");
+            Thread errorThread = new Thread(errorStreamReaderRunnable, "LogStreamReader error");
+            errorThread.start();
 
-	}
+            System.out.println("Waiting for finish...");
+            int exitCode = spark.waitFor();
+            System.out.println("Finished! Exit code:" + exitCode);
+            LOGGER.info("Spark Job Is Alive: " + spark.isAlive());
+
+        } catch (IOException e) {
+            LOGGER.info(e);
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            LOGGER.info(e);
+            e.printStackTrace();
+        }
+
+    }
 
 }
