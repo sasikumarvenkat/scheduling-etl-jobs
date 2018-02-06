2ba55f3e52161f49a46b2030d30954f9c2838bf8
Sasikumar Venkatesh <sasikumar.venkat@gmail.com>
2018-02-06 16:45:50 +0530

    [Scheduling-Spark] Scheduling Word Count Application using Spark and Spring

--- src/main/java/com/example/automation/schedulingetljobs/constants/SchedulerConstants.java
+++ src/main/java/com/example/automation/schedulingetljobs/constants/SchedulerConstants.java
@@ -3,5 +3,7 @@ package com.example.automation.schedulingetljobs.constants;
 public interface SchedulerConstants {
 
 	final String CRON_EXPRESSION_EVERY_MINUTE = " 0 0/1 * 1/1 * ?";
+
+	final String CRON_EXPRESSION_EVERY_DAY = " 0 0 12 1/1 * ?";
 }
  
--- src/main/java/com/example/automation/schedulingetljobs/taks/ScheduledTasks.java
+++ src/main/java/com/example/automation/schedulingetljobs/taks/ScheduledTasks.java
@@ -29,7 +29,7 @@ public class ScheduledTasks {
         LOGGER.info("Info: Scheduling the Apache Spark ETL workloads");
         Process spark;
         try {
-            spark = new SparkLauncher().setAppResource("//usr/local/spark/examples/jars/spark-examples_2.11-2.2.1.jar")
+            spark = new SparkLauncher().setAppResource("/usr/local/spark/examples/jars/spark-examples_2.11-2.2.1.jar")
                     .setMainClass("org.apache.spark.examples.SparkPi").setMaster("local").setAppName("Spark Pi")
                     .setConf(SparkLauncher.DRIVER_MEMORY, "1g").addAppArgs("100").launch();
             LOGGER.info("Spark Pi Started.");
@@ -53,6 +53,40 @@ public class ScheduledTasks {
             LOGGER.info(e);
             e.printStackTrace();
         }
+
+    }
+
+    @Scheduled(cron = SchedulerConstants.CRON_EXPRESSION_EVERY_DAY)
+    public void scheduleSparkWordCount() {
+        LOGGER.info("Info: Scheduling the Apache Spark ETL workloads - Word Count");
+        Process spark;
+        try {
+            spark = new SparkLauncher().setAppResource("sparktest_2.11-0.1.jar")
+                    .setMainClass("com.example.test").setMaster("local").setAppName("Spark Word Count")
+                    .setConf(SparkLauncher.DRIVER_MEMORY, "1g").addAppArgs("/home/sasi/Documents/spark/input.txt").
+                            addAppArgs("/home/sasi/Documents/sasi/output1").launch();
+            LOGGER.info("Spark Pi Started.");
+            InputStreamReaderRunnable inputStreamReaderRunnable = new InputStreamReaderRunnable(spark.getInputStream(), "input");
+            Thread inputThread = new Thread(inputStreamReaderRunnable, "LogStreamReader input");
+            inputThread.start();
+
+            InputStreamReaderRunnable errorStreamReaderRunnable = new InputStreamReaderRunnable(spark.getErrorStream(), "error");
+            Thread errorThread = new Thread(errorStreamReaderRunnable, "LogStreamReader error");
+            errorThread.start();
+
+            System.out.println("Waiting for finish...");
+            int exitCode = spark.waitFor();
+            System.out.println("Finished! Exit code:" + exitCode);
+            LOGGER.info("Spark Job Is Alive: " + spark.isAlive());
+
+        } catch (IOException e) {
+            LOGGER.info(e);
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            LOGGER.info(e);
+            e.printStackTrace();
+        }
+
 
     }
 
